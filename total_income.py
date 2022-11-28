dd6e3acf3294cac125ea46bfdcbbe7479df915e4
Sathvika-7284 <sathvika7276@gmail.com>
2022-11-28 15:20:10 +0800

    cumulative income

--- total_income.py
+++ total_income.py
@@ -9,16 +9,25 @@ def main():
     incomes = []
     months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
-        incomes.append(income)
+    append_income(incomes, months)
 
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
+    print_report(incomes, months, total)
+
+
+def print_report(incomes, months, total):
+    for number_of_months in range(1, months + 1):
+        income = incomes[number_of_months - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(number_of_months, income, total))
+
+
+def append_income(incomes, months):
+    for number_of_months in range(1, months + 1):
+        # income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {str(number_of_months)}:"))
+        incomes.append(income)
 
 
 main()
