f50a8a060e6ae53b6418504a82530ed45716b850
Sathvika-7284 <sathvika7276@gmail.com>
2022-11-28 14:48:09 +0800

    indexing and slicing lists

---
+++ lists_warmup.py
@@ -0,0 +1,24 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] - Answer: 3
+# numbers[-1] - Answer: 2
+# numbers[3] - Answer: 1
+# numbers[:-1] - Answer: [3,1,4,1,5,9]
+# numbers[3:4] - Answer: [1]
+# 5 in numbers - Answer: True
+# 7 in numbers - Answer: False
+# "3" in numbers - Answer: False
+# numbers + [6, 5, 3] - Answer: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# 1.
+numbers[0] = "ten"
+print(numbers)
+
+# 2.
+numbers[-1] = 1
+print(numbers)
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
