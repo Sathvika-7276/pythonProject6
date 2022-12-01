66cc83c21803795615fa187716fa66deb485b5a1
Sathvika-7284 <sathvika7276@gmail.com>
2022-12-02 03:22:26 +0800

    working with list accessing and functions

--- list_exercises.py
+++ list_exercises.py
@@ -8,3 +8,12 @@ print(f"The last number is {Numbers[-1]}")
 print(f"The smallest number is {min(Numbers)}")
 print(f"The largest number is {max(Numbers)}")
 print(f"the average of the numbers is {sum(Numbers)/len(Numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface','BaseStdIn','Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter your name:")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
